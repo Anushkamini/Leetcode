2434b875c0cb29bea261bc7cb2b1c074ce42b480
Anushkamini <anushkamini01@gmail.com>
2026-07-11 22:46:29 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int climbStairs(int n) {
-      if(n<=2)
-      return n;
+    if(n<=2)
+    return n;
+    int first = 1;
+    int second =2;
 
-      int first=1;
-      int second=2;
+    for(int i=3; i<=n ; i++){
+        int current= first + second;
+        first = second;
+        second= current;
 
-      for(int i=3 ; i<=n ; i++){
-        int current= first+ second;
-        first= second;
-        second= current ;
-      }  
-      return second;
+    }
+    return second;
+
+
+
     }
 };
+ 
