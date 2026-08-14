bdf642dd5c1f6cff75b76fe12d6c0903d5794626
Anushkamini <anushkamini01@gmail.com>
2026-08-15 00:44:19 +0530

    Time: 0 ms (100%), Space: 9.3 MB (68.97%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -6,7 +6,6 @@ public:
         int left = 0;
 
         for (int right = 0; right < s.length(); ++right) {
- 
             int currentCharIndex = s[right] - 'a';
             ++charCount[currentCharIndex];
             while (charCount[currentCharIndex] > 2) {
@@ -14,11 +13,9 @@ public:
                 --charCount[leftCharIndex];
                 ++left;
             }
-
             int currentWindowSize = right - left + 1;
             maxLength = max(maxLength, currentWindowSize);
-        }
-        
+        }  
         return maxLength;  
     }
 };
